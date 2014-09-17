02aaab61c34ece0663ce069242e85cbff2e99fd0
Heberth Braga <heberth@quimbik.com>
2014-09-17 13:31:45 -0300

    Updated README file

--- DrawingApp/ViewController.swift
+++ DrawingApp/ViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var messageTextField: UITextField!
+    
+    @IBOutlet weak var mailBtn: UIButton!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +30,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func sendMailBtn(sender: UIButton) {
+        
+        messageLabel.hidden = false
+        messageLabel.text = messageTextField.text
+        messageLabel.textColor = UIColor.redColor()
+        
+        messageTextField.text = ""
+        messageTextField.resignFirstResponder()
+        
+        mailBtn.setTitle("Mail Sent", forState: UIControlState.Normal)
+        
+        nameLabel.hidden = false
+        nameLabel.text = nameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+        
+        nameTextField.text = ""
+    }
 }
-
